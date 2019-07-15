2250ed9cf05c37c82af81e40c1ca39c2d4b61dd6
crackmech <crack.mech@gmail.com>
2019-07-15 09:02:18 +0530

    video parallel process

--- imageLegTwitchBaseFunctions_tmp_20190713.py
+++ imageLegTwitchBaseFunctions_tmp_20190713.py
@@ -32,8 +32,10 @@ from thread import start_new_thread as startNT
 import os
 import tkFileDialog as tkd
 import Tkinter as tk
+import multiprocessing as mp
+import itertools
 
-#import time
+import time
 #import math
 #import matplotlib.pyplot as plt
 
@@ -224,10 +226,13 @@ def getTemplate(imData, roi):
     Returns the template from the given image data using roi values
     '''
     template = imData[roi[2]:roi[3], roi[0]:roi[1]]
-    if imData.shape[-1]<3:
-        return template
+    if len(imData.shape)>2:
+        if imData.shape[-1]<3:
+            return template
+        else:
+            return cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
     else:
-        return cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+        return template
 
 def trackTemplate(template, roi, imData, trackImSpread):
     '''
@@ -255,6 +260,12 @@ def displayImageWithROI(windowName, imData, roilist, imArgs):
         cv2.rectangle(imData, (roi[0], roi[2]), (roi[1], roi[3]), imArgs['roiColors'][i], imArgs['roiBorderThickness'])
     img = resizeImage(imData, imArgs['imResizeFactor'])
     cv2.imshow(windowName, img)
+
+def displayIm(windowName, imData, sleepTime):
+    '''
+    '''
+    cv2.imshow(windowName, imData)
+    return cv2.waitKey(sleepTime) & 0xFF
 
 
 #---------------------------------------------------------------------------------#
@@ -393,18 +404,17 @@ def getFrameFromVideo(vfname, frameN):
     else:
         print('No frame at given position')
 
-def displayVid(vfname, roilist, imArgs):
+def displayVid(vfname, roilist, imArgs, fps):
     '''
     Starts displaying Video from the input video file,
     update the ROIs by pressing 'u'
     '''
     cap = cv2.VideoCapture(vfname)
-    
     while(cap.isOpened()):
         ret, imData = cap.read()
         if ret:
-            displayImageWithROI('Displaying Video', imData, roilist, imArgs)
-            k = cv2.waitKey(100) & 0xFF
+            displayImageWithROI('Press "u" to update ROIs', imData, roilist, imArgs)
+            k = cv2.waitKey(int(1000/fps)) & 0xFF
             if k == ord('q'):
                 break
             if k == ord("u"):
@@ -416,51 +426,235 @@ def displayVid(vfname, roilist, imArgs):
     cv2.destroyAllWindows()
     return roilist
 
-def decodeNProc(vfname, roilist, templatelist, imArgs):
+def decodeNProc(vfname, roilist, displayfps, imArgs):
     '''
-    Function for capturing images from the camera and then tracking already defined
-    templates. The template images are updated (using ROIs defined earlier)
-    everytime the function is called. Pressing 'Ctrl+c' pauses the tracking loop
-    and starts displaying live images from the camera. This can be used to select
-    new templates while the function is running.
+    returns the tracking data for the selected ROIs from the video file
     '''
     logfname = imArgs['logfname']
-    print("%s Press 'Ctrl+C' to pause analysis and start live display"%present_time())
-    legCoords = np.zeros((nFrames,2*imArgs['nRois']), dtype = 'uint16')
     logFileWrite(logfname, present_time(), printContent = False)
-    for nFrame in range (0,nFrames):
-        try:
+    print("%s Press 'p' to pause analysis and start live display"%present_time())
+    cap = cv2.VideoCapture(vfname)
+    nFrames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    legCoords = np.zeros((nFrames,2*imArgs['nRois']), dtype = 'uint16')
+    nFrame = 0
+    while(cap.isOpened()):
+#        ret = cap.grab()
+        ret, imData = cap.read()
+        if ret:
+            imData = cv2.cvtColor(imData, cv2.COLOR_RGB2GRAY)
+            if nFrame == 0:
+                templatelist = [getTemplate(imData, roi) for roi in roilist]
+                print('Got the templates')
+            for i, template in enumerate(templatelist):
+                legCoords[nFrame, i:i+2] = trackTemplate(template, roilist[i], imData, imArgs['trackImSpread'])
+            nFrame += 1
             if nFrame%100 == 0:
                 sys.stdout.write("\r%s: %d"%(present_time(),nFrame))
                 sys.stdout.flush()
-            c.retrieve_buffer(im)
-            imData = np.array(im)
-            if nFrame == 10:
-                roilist = selRoi(imData, roilist, imArgs, \
-                       getROI = False, showRoiImage = False, saveRoiImage = True)
-                templatelist = [getTemplate(imData, roi) for roi in roilist]
-                if np.median(imData)>250: # to stop imaging if the image goes white predominantely
-                    #print("\n-------No pupa to Image anymore!!-------")
-                    logFileWrite(logfname, "------- No pupa to image anymore!! Imaging exited -------", printContent = True)
-                    sys.exit(0)
-            for i, template in enumerate(templatelist):
-                legCoords[nFrame, i:i+2] = trackTemplate(template, roilist[i], imData, imArgs['trackImSpread'])
-            #print('\ni: %d\n nFrame: %d\ntemplateList length: %d\n roiList length: %d'%(i,nFrame, len(templatelist), len(roilist)))
-            if saveIm == True:
-                try:
-                    startNT(cv2.imwrite,(os.path.join(saveDir, str(nFrame)+'.jpeg'),imData,))
-                except:
-                    print("error saving %s"+str(nFrame))
-            elif saveIm == False:
-                if nFrame%1000 == 0:
-                    startNT(cv2.imwrite,(os.path.join(saveDir, str(nFrame)+'.jpeg'),imData,))
-        except KeyboardInterrupt:
-            logFileWrite(logfname, "\nCamera display started on %s"%present_time(), printContent = True)
-            roilist = displayCam(c, im, roilist, imArgs)
-            logFileWrite(logfname, "Camera display exited on %s"%present_time(), printContent = False)
+            k = displayIm('Template Tracking', resizeImage(imData, 0.1), 1)
+            if k == ord('p'):
+                print('\nPressed "p" at frame #%d'%nFrame)
+                roilist = displayVid(vfname, roilist, imArgs, displayfps)
+        else:
+            cap.release()
+            cv2.destroyAllWindows()
     logFileWrite(logfname, present_time(), printContent = False)
     logFileWrite(logfname, '----------------------', printContent = False)
     values = legCoords#np.c_[l1,r1,l2,r2,bg,bg1]
     return values
+  
+def trackAllTemplates(templatelist, roilist, imArgs, imData):
+    '''
+    '''
+    trackCoords = np.zeros(2*len(templatelist), dtype=np.uint16)
+    for i, template in enumerate(templatelist):
+        trackCoords[i:i+2] = trackTemplate(template, roilist[i], imData, imArgs['trackImSpread'])
+    return trackCoords
+    
+def procVideo((vfname, roilist, templatelist, frameStep, imArgs, group_number)):
+    cap = cv2.VideoCapture(vfname)
+    cap.set(cv2.CAP_PROP_POS_FRAMES, frameStep * group_number)
+    legCoords = np.zeros((frameStep,2*imArgs['nRois']), dtype = np.uint16)
+    proc_frames = 0
+    print('Started processing on processor: %d' %group_number)
+    while proc_frames < frameStep:
+        ret, imData = cap.read()
+        if ret:
+            imData = cv2.cvtColor(imData, cv2.COLOR_RGB2GRAY)
+        legCoords[proc_frames] = trackAllTemplates(templatelist, roilist, imArgs, imData)
+        proc_frames += 1
+    cap.release()
+    return legCoords
+
+def decodeNProcParllel(vfname, roilist, displayfps, imArgs, nThreads):
+    '''
+    returns the tracking data for the selected ROIs from the video file
+    '''
+    logfname = imArgs['logfname']
+    logFileWrite(logfname, present_time(), printContent = False)
+    startTime = time.time()
+    print("Started processing at: %s"%present_time())
+    cap = cv2.VideoCapture(vfname)
+    frameStep =  int(cap.get(cv2.CAP_PROP_FRAME_COUNT) // nThreads)
+    ret, imData = cap.read()
+    if ret:
+        imData = cv2.cvtColor(imData, cv2.COLOR_RGB2GRAY)
+        templatelist = [getTemplate(imData, roi) for roi in roilist]
+        mpArgs = itertools.izip(itertools.repeat(vfname), \
+                                itertools.repeat(roilist), \
+                                itertools.repeat(templatelist), \
+                                itertools.repeat(frameStep), \
+                                itertools.repeat(imArgs), \
+                                range(nThreads)
+                                )
+        legCoords = mp.Pool(nThreads).map(procVideo, mpArgs)
+        legCoordsStack = np.zeros((len(legCoords)*legCoords[0].shape[0], legCoords[0].shape[1]), dtype=np.uint16)
+        for i, coords in enumerate(legCoords):
+            legCoordsStack[i*frameStep:(i+1)*frameStep] = coords
+        print(len(legCoords), legCoords[0].shape, legCoordsStack.shape, legCoordsStack.max(), legCoordsStack.min())
+        print('Done processing in %0.2f Seconds, at: %s' %(time.time()-startTime,present_time()))
+        cap.release()
+        return legCoordsStack
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def decodeNProcParllel1(vfname, roilist, displayfps, imArgs):
+    '''
+    returns the tracking data for the selected ROIs from the video file
+    '''
+    logfname = imArgs['logfname']
+    logFileWrite(logfname, present_time(), printContent = False)
+    print("%s Press 'p' to pause analysis and start live display"%present_time())
+    cap = cv2.VideoCapture(vfname)
+    nFrames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frameheight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    imStack = np.zeros((nFrames, frameheight, frameWidth), dtype = np.uint8)
+    nFrame = 0
+    while(cap.isOpened()):
+#        ret = cap.grab()
+        ret, imData = cap.read()
+        if ret:
+            imData = cv2.cvtColor(imData, cv2.COLOR_RGB2GRAY)
+            imStack[nFrame] = imData
+            nFrame += 1
+            if nFrame%100 == 0:
+                sys.stdout.write("\r%s: %d"%(present_time(),nFrame))
+                sys.stdout.flush()
+        else:
+            cap.release()
+    legCoords = np.zeros((nFrames,2*imArgs['nRois']), dtype = 'uint16')
+    print('\nread Images, now tracking at: %s'%present_time())
+    for i, imData in enumerate(imStack):
+        if i == 0:
+            templatelist = [getTemplate(imData, roi) for roi in roilist]
+            print('Got the templates')
+        legCoords[i] = trackAllTemplates(templatelist, roilist, imArgs, imData)
+    print('Tracked all images at: %s'%present_time())
+#        for i, template in enumerate(templatelist):
+#            legCoords[nFrame, i:i+2] = trackTemplate(template, roilist[i], imData, imArgs['trackImSpread'])
+    return legCoords
+#            k = displayIm('Template Tracking', resizeImage(imData, 0.1), 1)
+#            if k == ord('p'):
+#                print('\nPressed "p" at frame #%d'%nFrame)
+#                roilist = displayVid(vfname, roilist, imArgs, displayfps)
+#        else:
+#            cap.release()
+#            cv2.destroyAllWindows()
+#    logFileWrite(logfname, present_time(), printContent = False)
+#    logFileWrite(logfname, '----------------------', printContent = False)
+#    values = legCoords#np.c_[l1,r1,l2,r2,bg,bg1]
+#    return values
    
+    
+
+##    itertools.izip(flist, itertools.repeat(params), np.arange(len(flist)))
+#    nFrame = 0
+#    while(cap.isOpened()):
+##        ret = cap.grab()
+#        ret, imData = cap.read()
+#        if ret:
+#            imData = cv2.cvtColor(imData, cv2.COLOR_RGB2GRAY)
+#            imStack[nFrame] = imData
+#            nFrame += 1
+#            if nFrame%100 == 0:
+#                sys.stdout.write("\r%s: %d"%(present_time(),nFrame))
+#                sys.stdout.flush()
+#        else:
+#            cap.release()
+#    legCoords = np.zeros((nFrames,2*imArgs['nRois']), dtype = 'uint16')
+#    print('\nread Images, now tracking at: %s'%present_time())
+#    for i, imData in enumerate(imStack):
+#        if i == 0:
+#            templatelist = [getTemplate(imData, roi) for roi in roilist]
+#            print('Got the templates')
+#        legCoords[i] = trackAllTemplates(templatelist, roilist, imArgs, imData)
+#    print('Tracked all images at: %s'%present_time())
+##        for i, template in enumerate(templatelist):
+##            legCoords[nFrame, i:i+2] = trackTemplate(template, roilist[i], imData, imArgs['trackImSpread'])
+#    return legCoords
+#
+
+
+#            k = displayIm('Template Tracking', resizeImage(imData, 0.1), 1)
+#            if k == ord('p'):
+#                print('\nPressed "p" at frame #%d'%nFrame)
+#                roilist = displayVid(vfname, roilist, imArgs, displayfps)
+#        else:
+#            cap.release()
+#            cv2.destroyAllWindows()
+#    logFileWrite(logfname, present_time(), printContent = False)
+#    logFileWrite(logfname, '----------------------', printContent = False)
+#    values = legCoords#np.c_[l1,r1,l2,r2,bg,bg1]
+#    return values
+
+
      
+
+
+
+
+
--- legTwitch_TrackVideo_20190714.py
+++ legTwitch_TrackVideo_20190714.py
@@ -12,7 +12,6 @@ import numpy as np
 import os
 import glob
 
-
 try: input = raw_input
 except NameError: pass
 
@@ -21,22 +20,16 @@ saveRoiImage = False
 
 #dirname = '/home/pointgrey/imaging/'
 ##dirname = '/media/pointgrey/4TB_2/'
-#secondaryDisk = '/media/pointgrey/shared/'
-#home = '/home/pointgrey/'
-home = '/home/aman/'
 dirname = '/media/aman/data/'
 
-vidDir = ''
 #pupaDetails = input('Enter the pupa details : <genotype> - <APF> - <time> - <date>\n')
 imResizeFactor = 0.5 #resize image to this factor for display feed of the camera
 templateResizeFactor = 4 #resize image to this factor for display of the template
 nLegs = 4 # no. of legs to be tracked
 nBgs = 2 # no. of background templates to be tracked
-imDuration = 20      #in minutes
-trackImSpread = 30  #number of pixles around ROI where the ROI will be tracked
+trackImSpread = 100  #number of pixles around ROI where the ROI will be tracked
 
-
-genotype = ''#pupaDetails.split(' -')[0]
+genotype = 'VideoTmp'#pupaDetails.split(' -')[0]
 imfolder = 'imageData'
 roifolder = 'roi'
 csvfolder = 'csv'
@@ -48,8 +41,6 @@ except:
     sys.exit()
 
 roiVal = [504, 510, 110, 116]
-templateList = [np.zeros((10,10), dtype=np.uint8)]
-
 roiColors = [(255,0,0), (0,255,0), (0,0,255), (255,255,0), (255,0,255)]
 roiBorderThickness = 2
 
@@ -59,17 +50,9 @@ templateKeyDict = {1: 'leg L1', 2: 'leg R1', \
                    }        
 
 nRois = nLegs+nBgs
-roiSelKeys = [ord(str(x)) for x in range(nRois)] # keys to be pressed on keyboard for selecting the roi
 
 logFileName = os.path.join(baseDir, "camloop.txt")
-#
-#imLegTw.logFileWrite(logFileName, pupaDetails, printContent = False)
 roiList = imLegTw.selPreviousRois(roiDir, roiVal, nRois)
-
-SaveDirDuration = int(120/imDuration)
-nFrames = int((imDuration*60*100)+1)
-nFrames = 601
-print(nFrames)
 
 imLegTw.logFileWrite(logFileName, '----------------------', printContent = False)
 imArgs = {'nLegs': nLegs,
@@ -88,32 +71,16 @@ imArgs = {'nLegs': nLegs,
           'templateKeys': templateKeyDict
            }
 
-vidDir = '/media/aman/data/20190714_050816_12/imageData/'
+vidDir = '/media/aman/data/work/pupaData/20190107_212915_34750TMP/imageData'
 vidFlist = glob.glob(os.path.join(vidDir, '*.avi'))
 
-for vidFname in vidFlist:
+for nVid, vidFname in enumerate(vidFlist):
     roiList = imLegTw.roiSelVid(vidFname, roiList, imArgs, getROI = True)
-    roiList = imLegTw.displayVid(vidFname, roiList, imArgs)
-    
-    roiFileName = roiDir+"ROIs_"+imLegTw.present_time()+".txt"
-    np.savetxt(roiFileName, roiVal, fmt = '%d', delimiter = ',')
-    
-    imData = imLegTw.getFrameFromVideo(vidFname, 1)
-    print(imData.shape)
-    templateList = [imLegTw.getTemplate(imData, roi) for roi in roiList]
-
-
-
-#for i, vidFname in enumerate(vidFlist):
-#    imLegTw.logFileWrite(logFileName, "Directory : "+saveDir, printContent = True)
-#    trackedValues = imLegTw.CapNProc(c, im, roiList, templateList, nFrames, saveDir, saveImData, imArgs)
-#    np.savetxt(os.path.join(csvDir, dirTime+"_XY.csv"), trackedValues, fmt = '%-7.2f', delimiter = ',')
-#    print("\r\nProcesing for loop number: "+str(nLoop+1))
-#
-#c.stop_capture()
-#c.disconnect()
-#
-#imLegTw.logFileWrite(logFileName, '----------------------', printContent = False)
-
+    dirTime = imLegTw.present_time()
+    print(dirTime)
+    imLegTw.logFileWrite(logFileName, "Video file : %s"%vidFname, printContent = True)
+    trackedValues = imLegTw.decodeNProcParllel(vidFname, roiList, displayfps=100, imArgs = imArgs, nThreads=4)
+    np.savetxt(os.path.join(csvDir, dirTime+"_XY.csv"), trackedValues, fmt = '%-7.2f', delimiter = ',')
+    print("\r\nProcesing for video file %d/%d: "%(nVid, len(vidFlist)))
 
 
